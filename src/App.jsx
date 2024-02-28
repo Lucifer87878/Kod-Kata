38f6e7893617981f068f263a6c55806b1f8d02e1
Goran P <nallenrockar123@gmail.com>
2024-02-28 13:35:40 +0100

    steg 4

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+
+const App = () => {
+    return (
+        <>
+        
+        <Header headerText="Steg 4 avklarat" />
+        </>
+
+    )
+};
+
+export default App;
