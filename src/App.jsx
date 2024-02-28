92c5cdcbdf09c008c56ed25c2b8cbf881abc7afd
Goran P <nallenrockar123@gmail.com>
2024-02-28 14:30:10 +0100

    uppgift 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Header from "./components/Header";
 import RepeatText from "./components/RepeatText";
+import List from "./components/List";
+
+
 const App = () => {
     return (
         <>
         
         <Header headerText="Steg 4 avklarat" />
         <RepeatText count="7" text="EN liten text" />
+        <List animalArray={["Häst", "Ko", "Hund", ]}/>
         </>
 
     )
